72dee55ca3f15e4ba16b8d14b24208265256b4de
Yogu <info@yogularm.de>
2013-03-30 15:43:54 +0100

    switched from websockets to sockets.io

--- controller.js
+++ controller.js
@@ -1,67 +1,81 @@
 var contacts = {};
-var clients = [];
+var sockets = [];
 
-function accept(client) {
-	var query = client.request.resourceURL.query;
-	if (!query.user) {
-		client.send('error', {message: 'specify user name'});
-		client.close();
-		return;
-	}
+function accept(socket) {
+	var isLoggedIn = false;
+	var contact = null;
+	var user = null;
 	
-	var name = query.user;
-	var contact = addContact(name);
-	if (!contact) {
-		client.send('name_not_available');
-		client.close();
-		return;
-	}
-	
-	client.send('accepted');
-	client.contact = contact;
-	contact.client = client;
-	clients.push(client);
-	sendContacts(client);
-	console.log(contact.name + ' logged in');
-	
-	client.on('message', function(type, data) {
-		switch (type) {
-		case 'call':
-		case 'accept':
-		case 'reject':
-		case 'candidate':
-		case 'close':
-		case 'data':
-			if (data.contact && data.contact in contacts) {
-				var contact = contacts[data.contact];
-				var client = contact.client;
-				console.log(type + ' from ' + name + ' to ' + data.contact);
-				data.contact = name; // was recipient, becomes sender in the answer
-				client.send(type, data);
-			} else
-				console.log('invalid call/accept/reject/candidate/close/data message');
-			break;
-		default:
-			console.log('invalid message type: ' + type);
+	socket.on('login', function(data) {
+		if (isLoggedIn) {
+			socket.emit('error', {message: 'already logged in'});
+			socket.disconnect();
+			return;
 		}
+		
+		user = (data.user || "").trim();
+		if (!user) {
+			socket.emit('error', {message: 'specify user name'});
+			socket.disconnect();
+			return;
+		}
+		
+		contact = addContact(user);
+		if (!contact) {
+			client.emit('name_not_available');
+			client.disconnect();
+			return;
+		}
+		
+		socket.emit('accepted');
+		socket.contact = contact;
+		contact.socket = socket;
+		sockets.push(socket);
+		sendContacts(socket);
+		console.log(contact.name + ' logged in');
+		isLoggedIn = true;
 	});
+	
+	var events = ['call', 'accept', 'reject', 'candidate', 'close', 'data'];
+	
+	for (var i = 0; i < events.length; i++) {
+		(function(type) {
+			socket.on(type, function(data) {
+				if (!isLoggedIn) {
+					socket.emit('error', {message: 'log in first'});
+					return;
+				}
+				
+				if (data.contact && data.contact in contacts) {
+					var recipient = contacts[data.contact];
+					var recipientSocket = recipient.socket;
+					console.log(type + ' from ' + user + ' to ' + data.contact);
+					data.contact = user; // was recipient, becomes sender in the answer
+					recipientSocket.emit(type, data);
+				} else
+					console.log('invalid message: contact parameter missing (from ' + user + ')');
+			});
+		})(events[i]);
+	}
 
-	client.on('close', function() {
-		var index = clients.indexOf(client);
+	socket.on('disconnect', function() {
+		var index = sockets.indexOf(socket);
 		if (index >= 0)
-			clients.slice(index, 1);
-		removeContact(contact);
-		console.log(contact.name + ' logged out');
+			sockets.slice(index, 1);
+		if (contact != null) {
+			removeContact(contact);
+			console.log(contact.name + ' logged out');
+		}	
 	});
 };
 
-function sendContacts(client) {
-	client.send('contacts', Object.keys(contacts));
+function sendContacts(socket) {
+	socket.emit('contacts', Object.keys(contacts));
 }
 
-function sendContactsToAllClients() {
-	for (var i = 0; i < clients.length; i++) {
-		sendContacts(clients[i]);
+function sendContactsToAllSockets() {
+	for (var i = 0; i < sockets.length; i++) {
+		sendContacts(sockets[i]);
 	}
 }
 
@@ -71,7 +85,7 @@ function addContact(name) {
 	else {
 		var contact = {name: name};
 		contacts[name] = contact;
-		sendContactsToAllClients();
+		sendContactsToAllSockets();
 		return contact;
 	}
 }
@@ -79,7 +93,7 @@ function addContact(name) {
 function removeContact(contact) {
 	if (contact.name in contacts) {
 		delete contacts[contact.name];
-		sendContactsToAllClients();
+		sendContactsToAllSockets();
 	}
 }
 
